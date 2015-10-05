68e73d4c3b0e0676c79cd7b6434d5a9585b744b9
Ryutaro YOSHIBA <ryuzee@gmail.com>
2015-10-05 09:00:22 +0900

    fix typo

--- install.sh
+++ install.sh
@@ -74,10 +74,10 @@ cd /tmp && unzip app.zip
 REL=`date +%Y%m%d%H%M%S`
 mkdir -p /var/www/application/releases/$REL
 cp -Rp /tmp/open-slideshare-master/ -T /var/www/application/releases/$REL
-chown -R $DELOY_USER:$DEPLOY_GROUP /var/www/application/releases/$REL
+chown -R $DEPLOY_USER:$DEPLOY_GROUP /var/www/application/releases/$REL
 if [ ! -n "`readlink /var/www/application/current`" ]; then rm -rf /var/www/application/current; fi
 ln -s /var/www/application/releases/$REL /var/www/application/current
-chown $DELOY_USER:$DEPLOY_GROUP /var/www/application/current
+chown $DEPLOY_USER:$DEPLOY_GROUP /var/www/application/current
 chmod -R 777 /var/www/application/current/app/tmp/
 cd /var/www/application/current && php composer.phar install
 chmod 755 /var/www/application/current/app/Console/cake
